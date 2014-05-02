de5d4962642499da443e2b317a9f0e22db47f3be
Finn <derfinn@gmail.com>
2014-05-02 10:23:16 +0200

    Error handler

--- cli.js
+++ cli.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
 
 var http = require('http');
+var fs = require('fs');
 var send = require('send');
 
 var port = 2600;
+var root = process.cwd();
 
-var app = http.createServer(function(req, res){
+var errorHandler = function(err) {
+  res.statusCode = err.status || 500;
+  var errorPath = root + '/' + res.statusCode + '.html';
+  fs.createReadStream(errorPath)
+    .on('error', function() { res.end('Error ' + res.statusCode); })
+    .pipe(res)
+    ;
+};
+
+var requestHandler = function(req, res){
   send(req, req.url)
-    .from(process.cwd())
-    .pipe(res);
-}).listen(port);
+    .from(root)
+    .on('error', error)
+    .pipe(res)
+    ;
+}
+
+http.createServer(requestHandler).listen(port);
 
 console.log('Let\'s play video games!');
 console.log('>> Serving on port ' + port);
